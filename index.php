88651df3a7f4f95945c120a3ad6898b8dc892c76
Rubyii <84971065+Rubyii@users.noreply.github.com>
2022-11-28 17:49:31 +0100

    Grundgeruest richtige werte. Keine funktion

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+$bahnhoefe = array(
+    array("AachenHauptbahnhof", 50.76763697947344, 6.0909034446067825),
+    array("AachenRotheErde", 50.77018640899706, 6.116490727116905),
+    array("Dueren", 50.80930664580822, 6.48204588509695),
+    array("KoelnHauptbahnhof", 50.943288440980105, 6.958548054110135),
+    array("KoelnEhrenfeld", 50.95172918094622, 6.91836526945143),
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Startseite</title>
+    <link rel="stylesheet" href="static/css/index.css">
+    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVx2QBmwxDzm52voQ6qwjaOgjQS32JXEE&libraries=places&callback=initAutocomplete"></script>
+</head>
+<body>
+<div class="grid-container">
+    <div class="ueberschrift">
+        <img alt="ISLM-Bahn" class="logo" src="static/images/ISLM_Logo2009.png">
+        <div id="uhrzeitdatum" class="uhrzeitdatum">
+            <div id="uhrzeit"></div>
+            <div id="datum"></div>
+        </div>
+    </div>
+    <div class="hinweistext">
+        <div class="text">
+            Bitte wählen sie ein Tarif aus
+        </div>
+    </div>
+
+    <div class="hauptfunktion">
+        <div class="hauptfunktion-innen">
+            <form action="test.php" method="post">
+
+
+                <input type="submit" class="input" value="Weiter">
+            </form>
+        </div>
+    </div>
+
+    <div class="navigation">
+        <div class="navigation-innen">
+
+        </div>
+    </div>
+</div>
+</body>
+<script src="static/js/zeit.js"></script>
+</html>
